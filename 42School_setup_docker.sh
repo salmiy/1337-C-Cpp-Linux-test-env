b7b28f110f7db124c2b8971c97c41253380ef1de
Salmi Youssef <salmi19971@gmail.com>
2023-08-12 13:03:58 +0100

    add setup docker script

---
+++ 42School_setup_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /goinfre/ysalmi/docker
+rm -rf /Users/ysalmi/Library/Containers/com.docker.docker
+ln -s /goinfre/ysalmi/docker /Users/ysalmi/Library/Containers/com.docker.docker
